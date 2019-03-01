1d6a61aa0bca37486d1c11f45eeffe9a91de2547
zaratustrat <44696350+zaratustrat@users.noreply.github.com>
2019-03-01 18:18:54 +0200

    main+

---
+++ active/js/script.js
@@ -0,0 +1,26 @@
+// $(document).ready(function () {
+//  var link = $('.menu-link');
+//  var link_active = $('.menu-link_active');
+//  var menu = $('.header');
+
+//  link.click(function () {
+//   link.toggleClass('menu-link_active');
+//   menu.toggleClass('menu_active');
+//  });
+//  link_active.click(function () {
+//   link.removeClass('menu-link_active');
+//  });
+// }); 
+
+// $(document).ready(function () {
+//  $('.callback-text').on('click', function () {
+//   $('#modal-callback').toggleClass('modal_visible');
+//  });
+//  $('.modal-close').on('click', function () {
+//   $('#modal-callback').toggleClass('modal_visible');
+//  });
+// });
+
+$(document).ready(function () {
+ $(".mask").mask("+7 (999) 99-99-999");
+})
