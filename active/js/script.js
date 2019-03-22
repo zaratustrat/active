05590498faa108ab73913c9ef01b98bdaddcbb7e
zaratustrat <44696350+zaratustrat@users.noreply.github.com>
2019-03-22 09:36:11 +0200

    php

--- active/js/script.js
+++ active/js/script.js
@@ -1,25 +1,12 @@
-// $(document).ready(function () {
-//  var link = $('.menu-link');
-//  var link_active = $('.menu-link_active');
-//  var menu = $('.header');
 
-//  link.click(function () {
-//   link.toggleClass('menu-link_active');
-//   menu.toggleClass('menu_active');
-//  });
-//  link_active.click(function () {
-//   link.removeClass('menu-link_active');
-//  });
-// }); 
-
-// $(document).ready(function () {
-//  $('.callback-text').on('click', function () {
-//   $('#modal-callback').toggleClass('modal_visible');
-//  });
-//  $('.modal-close').on('click', function () {
-//   $('#modal-callback').toggleClass('modal_visible');
-//  });
-// });
+$(document).ready(function () {
+  $('.button-callback').on('click', function () {
+  $('.modal').toggleClass('modal_visible');
+ });
+ $('.modal-close').on('click', function () {
+  $('.modal').toggleClass('modal_visible');
+ });
+});
 
 $(document).ready(function () {
  $('.mask').mask("+7 (999) 99-99-999");
@@ -31,13 +18,6 @@ $('.scrol').bind("click", function (e) {
   }, 1200);
   e.preventDefault();
 });
-// var $page = $('html, body');
-// $('.scrol').click(function () {
-//   $page.animate({
-//     scrollTop: $($.attr(this, 'href')).offset().top
-//   }, 400);
-//   return false;
-// });
 $('.slider').slick({
   slidesToShow: 1,
   slidesToScroll: 1,
